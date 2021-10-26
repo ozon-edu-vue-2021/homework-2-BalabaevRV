28463cf9980345149782d5f5f74ec62a83e58ba7
BalabaevRV <ice187boy@gmail.com>
2021-10-26 18:43:38 +0300

    Решение д/З

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,95 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Дерево файлов</h1>
+    <ul>
+      <directory-vue :structure="items"/>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import structure from "/public/static/node_modules.json";
+import directoryVue from "./components/directory";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    directoryVue
+  },
+  data () {
+    return {
+      items: structure,
+      currentTab: 1
+    }
+  },
+  methods: {
+    getNextEl (currentTabEl) {
+      let nextTabEl = currentTabEl.nextElementSibling;
+      if (!nextTabEl) {
+        nextTabEl = currentTabEl.parentElement;
+        if (nextTabEl.tagName === "LI") {
+          nextTabEl = nextTabEl.nextElementSibling;
+        }
+        if (!nextTabEl) {
+          nextTabEl = currentTabEl.parentElement.parentElement.parentElement.nextElementSibling;
+        }
+        if (!nextTabEl) {
+          return nextTabEl;
+        }
+      }
+      if (nextTabEl.tagName === "UL") {
+        if (!(nextTabEl.style.display === "none")) {
+          nextTabEl = nextTabEl.firstElementChild;
+        } else {
+          nextTabEl = nextTabEl.parentElement.nextElementSibling;
+        }
+      }
+      if(!nextTabEl) {
+        return nextTabEl;
+      }
+      if (nextTabEl.tagName === "LI") {
+        nextTabEl = nextTabEl.firstElementChild;
+      }
+      return nextTabEl;
+    },
+    getPreviousEl (currentTabEl) {
+      let previousTabEl = currentTabEl.parentElement.previousElementSibling;
+      if (!previousTabEl) {
+        previousTabEl = currentTabEl.parentElement.parentElement.previousElementSibling;
+      }
+      if (previousTabEl.tagName === "H1") {
+        return currentTabEl;
+      }
+      if (previousTabEl.tagName === "LI") {
+        previousTabEl = previousTabEl.lastElementChild;
+      }
+      if (previousTabEl.tagName === "UL") {
+        if (!(previousTabEl.style.display === "none")) {
+          previousTabEl = previousTabEl.lastElementChild.lastElementChild;
+        }
+        else {
+          previousTabEl = previousTabEl.previousElementSibling;
+        }
+      }
+      return previousTabEl;
+    },
+    changeFocus (e) {
+      if (e.key === "ArrowDown" || e.key === "ArrowUp") {
+        let nextTabEl = document.activeElement;
+        const getEl = (e.key === "ArrowDown") ? this.getNextEl : this.getPreviousEl;
+        if (nextTabEl.hasAttribute("data-vue-tab")) {
+            nextTabEl = getEl (nextTabEl);
+        } else {
+          nextTabEl = document.querySelector("[data-vue-tab]");
+        }
+        if (nextTabEl) {
+          nextTabEl.focus();
+        }
+      }
+    }
+  },
+  created() {
+    document.addEventListener("keydown", this.changeFocus);
   }
 }
 </script>
@@ -21,8 +99,17 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+ul {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+
+p {
+  margin: 0;
 }
 </style>
---
+++ src/components/directory.vue
@@ -0,0 +1,106 @@
+<template>
+  <li>
+    <button class="folderButton"  v-if="structure.type === 'directory'" @click="openDirectory = !openDirectory" tabindex="-1" data-vue-tab>
+      <span>{{structure.name}}</span>
+      <span class="path">{{currentPath}}</span>
+    </button>
+    <file-vue v-if="structure.type === 'file'" :title="structure.name" :currentPath="currentPath + structure.name"/>
+    <link-vue v-if="structure.type === 'link'" :title="structure.name" :currentPath="currentPath + structure.name" :targetLink="structure.target"/>
+    <ul v-if="structure.type === 'directory'" v-show="openDirectory" :style="{marginLeft: marginLeft + 'px'}">
+        <directory-vue
+            v-for="(item, index) in structure.contents"
+            :key="item + index"
+            :structure="item"
+            :marginLeft="marginLeft + 20"
+            :currentPath="currentPath + structure.name + '/'"
+        />
+    </ul>
+  </li>
+</template>
+
+<script>
+  import fileVue from "./file";
+  import linkVue from "./link";
+
+  export default {
+    name: "directoryVue",
+    components: {
+      fileVue,
+      linkVue
+    },
+    props: {
+      structure: Object,
+      type: String,
+      title: String,
+      Content: Array,
+      marginLeft:  {
+        type: Number,
+        default: 20
+      },
+      currentPath:  {
+        type: String,
+        default: "/"
+      }
+    },
+    data: function () {
+      return {
+        openDirectory: false,
+      }
+    },
+    computed: {
+      haveContent: function () {
+        if (typeof (this.structure.contents) === "object") {
+          return true;
+        }
+          return false;
+      }
+    }
+  }
+
+</script>
+
+<style scoped>
+
+  .folderButton {
+    font-weight: 700;
+    font-size: 20px;
+    background: none;
+    margin-top: 10px;
+    margin-bottom: 10px;
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+    box-sizing: border-box;
+    border: 0.5px solid transparent;
+    padding: 0;
+    width: 100%;
+  }
+
+  .folderButton:hover {
+    border: 0.5px solid lightblue;
+  }
+
+  .folderButton::before {
+    content: "";
+    background-image: url(../assets/folder.svg);
+    background-repeat: no-repeat;
+    background-size: cover;
+    width: 48px;
+    height: 48px;
+    display: block;
+  }
+
+  .folderButton:focus {
+    outline-color: lightblue;
+  }
+
+  .path {
+    margin-left: auto;
+    display: none;
+  }
+
+  .folderButton:hover .path{
+    display: block;
+  }
+
+</style>
